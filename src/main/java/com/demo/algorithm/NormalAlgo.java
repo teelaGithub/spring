05f7867ea64dc8d369b53c2be1805eab6b10e7c6
litinglan <18166038801@163.com>
2019-05-22 18:01:04 +0800

    'algorithm'

---
+++ src/main/java/com/demo/algorithm/NormalAlgo.java
@@ -0,0 +1,120 @@
+package com.demo.algorithm;
+
+import java.util.Random;
+
+/**
+ * 排序算法
+ *
+ * @author litinglan 2019/5/22 10:46
+ */
+public class NormalAlgo {
+    /**
+     * 冒泡排序：从小到大
+     * 相邻2个数比较，一趟比较下来最大的数排最后，在重新对数据比较排序
+     * 时间复制度：O(n^2)
+     */
+    public static void bubbleSort(int[] arr) {
+        int temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    /**
+     * 选择排序：每次找到未排序列最小/大值后直接与已排序列末尾交换（而不是每次都交换）
+     * 时间复制度：O(n^2)
+     */
+    public static void selectionSort(int[] arr) {
+        int mixIndex = 0, temp = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            //mixIndex 最小值位置，i 已排序列末尾
+            mixIndex = i;
+            //找出最小值的位置下标
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[mixIndex] > arr[j]) {
+                    mixIndex = j;
+                }
+            }
+            //直接和最小值交换
+            temp = arr[mixIndex];
+            arr[mixIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    /**
+     * 直接插入排序：对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入
+     * 已排序列比较时需往后移
+     * 时间复制度：O(n^2)
+     */
+    public static void insertionSort(int[] arr) {
+        int key = 0;//待排序数据
+        for (int i = 1; i < arr.length; i++) {
+            key = arr[i];
+            //与已排序列从后往前比较交换
+//            for (int j = i - 1; j >= 0; j--) {
+//                if (arr[j] > key) {
+//                    arr[j + 1] = arr[j];
+//                    arr[j] = key;
+//                }
+//            }
+            //网络答案
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    /**
+     * 归并排序：
+     */
+    public static void mergeSort(int[] arr) {
+        double floor = Math.floor(arr.length / 2);
+    }
+
+    /**
+     * 希尔排序：
+     */
+    public static void shellSort(int[] arr) {
+        int d = arr.length / 2;
+        while (d >= 1) {
+            for (int i = d; i < arr.length; i++) {
+                int j = i - d;
+                int temp = arr[i];    //记录要插入的数据
+                while (j >= 0 & arr[j] > temp) {  //从后向前，找到比其小的数的位置
+                    arr[j + d] = arr[j];    //向后挪动
+                    j -= d;
+                }
+                if (j != i - d)    //存在比其小的数
+                    arr[j + d] = temp;
+            }
+            d /= 2;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            arr[i] = random.nextInt(100);
+            System.out.printf("%2d ", arr[i]);
+        }
+        System.out.println();
+        NormalAlgo.shellSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%2d ", arr[i]);
+        }
+        System.out.println();
+    }
+}
--- src/main/java/com/demo/java/ExtendDemo.java
+++ src/main/java/com/demo/java/ExtendDemo.java
@@ -1,6 +1,6 @@
 package com.demo.java;
 
-import java.util.concurrent.Executors;
+import java.util.*;
 
 /**
  * @author litinglan 2019/5/16 11:44
@@ -21,8 +21,25 @@ public class ExtendDemo {
     }
 
     public static void main(String[] args) {
-        System.out.println(JuniorDemo.c);
-        System.out.println(new JuniorDemo().hashCode());
+//        System.out.println(JuniorDemo.c);
+
+        Random random = new Random(); // 创建随机数生成器
+        List list = new ArrayList();// 生成10 个随机数，并放在集合list 中
+        for (int i = 0; i < 10; i++) {
+            list.add(random.nextInt(1000));
+            System.out.println("ran: " + list.get(i));
+        }
+        Collections.sort(list);
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ": " + list.get(i));
+        }
+        // 对集合中的元素进行排序
+        Iterator it = list.iterator();
+        int count = 0;
+        while (it.hasNext()) {
+            // 顺序输出排序后集合中的元素
+            System.out.println(++count + ": " + it.next());
+        }
     }
 }
 
